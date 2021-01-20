54ebfcdaf198c13338dd57cc1d4a8b5edd196c4e
Emerson Pereira da Silva <pereiradasilva.emerson@gmailcom>
2021-01-19 23:53:10 -0300

    fix Deploy

--- dist/shared/infra/http/schema/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphqlImportFiles_1 = require("@shared/utils/graphqlImportFiles");
-exports.default = graphqlImportFiles_1.loadFiles('src/**/infra/http/schema/*.gql');
