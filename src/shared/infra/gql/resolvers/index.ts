814f1e3957ab21008fe7c2d3457b88ac210ba50b
uricbonatti <uricbonatti@gmail.com>
2021-01-27 08:50:47 -0300

    http folders changed name to gql folder

--- src/shared/infra/gql/resolvers/index.ts
+++ src/shared/infra/gql/resolvers/index.ts
@@ -1,16 +1,16 @@
 import ScalarResolver from './ScalarResolver';
 
-import { UserMutation, UserQuery } from '@modules/users/infra/http/resolvers';
-import { PostMutations, PostQuerys } from '@modules/posts/infra/http/resolvers';
+import { UserMutation, UserQuery } from '@modules/users/infra/gql/resolvers';
+import { PostMutations, PostQuerys } from '@modules/posts/infra/gql/resolvers';
 import {
   CategoriesQuerys,
   CategoriesMutations,
-} from '@modules/categories/infra/http/resolvers';
-import { TagsMutations, TagsQuerys } from '@modules/tags/infra/http/resolvers';
+} from '@modules/categories/infra/gql/resolvers';
+import { TagsMutations, TagsQuerys } from '@modules/tags/infra/gql/resolvers';
 import {
   CommentsMutations,
   CommentsQuerys,
-} from '@modules/comments/infra/http/resolvers';
+} from '@modules/comments/infra/gql/resolvers';
 
 const Query = {
   ...UserQuery,
--- src/shared/infra/http/schema/index.ts
+++
@@ -1,4 +0,0 @@
-import path from 'path';
-import { loadFiles } from '@shared/utils/graphqlImportFiles';
-
-export default loadFiles('src/**/infra/http/schema/*.gql');
